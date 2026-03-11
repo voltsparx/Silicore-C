d0681716b48a4fa4615ac6c11ccf192a28cadca5
Voltsparx <voltsparx@gmail.com>
2026-03-11 14:20:47 +0530

    Update banner.cpp

--- core/interface/banner.cpp
+++ core/interface/banner.cpp
@@ -11,12 +11,12 @@ namespace silicore::interface {
 
 void show_banner(const std::string& anonymity_status) {
     const std::vector<std::pair<const char*, const char*>> left_right_lines = {
-        {"       .d8888. d888888b db      d888888b  .o88b.  .d8b.", "          db    db"},
-        {"       88'  YP   `88'   88        `88'   d8P  Y8 d8' `8b", "         `8b  d8'"},
-        {"       `8bo.      88    88         88    8P      88ooo88", "          `8bd8'"},
-        {"         `Y8b.    88    88         88    8b      88~~~88  C8888D", "  .dPYb."},
-        {"       db   8D   .88.   88booo.   .88.   Y8b  d8 88   88", "         .8P  Y8."},
-        {"       `8888Y' Y888888P Y88888P Y888888P  `Y88P' YP   YP", "         YP    YP"},
+        {"       .d8888. d888888b db      d888888b  .o88b.  .d88b.  d8888b. d88888b", "          .o88b. "},
+        {"       88'  YP   `88'   88        `88'   d8P  Y8 .8P  Y8. 88  `8D 88'    ", "        d8P  Y8  "},
+        {"       `8bo.      88    88         88    8P      88    88 88oobY' 88ooooo", "        8P       "},
+        {"         `Y8b.    88    88         88    8b      88    88 88`8b   88~~~~~ C8888D", " 8b       "},
+        {"       db   8D   .88.   88booo.   .88.   Y8b  d8 `8b  d8' 88 `88. 88.    ", "        Y8b  d8  "},
+        {"       `8888Y' Y888888P Y88888P Y888888P  `Y88P'  `Y88P'  88   YD Y88888P", "         `Y88P'  "},
     };
 
     for (const auto& row : left_right_lines) {
